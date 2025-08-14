f53367a8d43449f12201d2cfef244b3da5271a9e
Nishanth <nishanthp03032005@gmail.com>
2025-08-14 11:42:36 +0530

    Delete sign-language-audio directory

--- sign-language-audio/services/translator.py
+++
@@ -1,6 +0,0 @@
-from googletrans import Translator
-
-def translate_text(text, target_lang='hi'):  # hi = Hindi
-    translator = Translator()
-    translated = translator.translate(text, dest=target_lang)
-    return translated.text
